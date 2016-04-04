1c72aae2974efe74eb5a7a9f7e2d43665706f2ed
Jonatan Lemos Zuluaga (Student) <jonatan.lemos.z@gmail.com>
2016-04-03 23:49:21 -0700

    Sorted file structure

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require("express");
-var userLib =  require("./user.js");
+var session = require("express-session");
+var userLib = require("./user.js");
 var app = express();
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || 'localhost';
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
+var methodOverride = require("method-override");
+var hostname = process.env.HOSTNAME || "localhost";
 var port = 8080;
-var db = require('./node_modules/mongoskin').db('mongodb://user:password@127.0.0.1:27017/RSSFEED');
+var db = require("./node_modules/mongoskin").db("mongodb://user:password@127.0.0.1:27017/RSSFEED");
+
+app.use(session({secret: "This is a secret"}));
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
@@ -19,9 +22,9 @@ app.get("/getAllPosts", function (req, res) {
     });
 });
 
-app.get('/listSubs', function(req, res){
+app.get("/listSubs", function(req, res){
       var info = req.query;
-      db.collection('subs').find({user:info.user}).toArray(function(err, result) {
+      db.collection("subs").find({user:info.user}).toArray(function(err, result) {
         if(err){
           res.send("[]")
         }
@@ -32,9 +35,9 @@ app.get('/listSubs', function(req, res){
 });
 
 
-app.get('/deleteSub', function(req, res){
+app.get("/deleteSub", function(req, res){
       var info = req.query;
-      db.collection('subs').remove({user:info.user, id:info.id}, function(err, result) {
+      db.collection("subs").remove({user:info.user, link: info.link}, function(err, result) {
         if(err){
           res.send("0")
         }
@@ -44,9 +47,9 @@ app.get('/deleteSub', function(req, res){
       });
 });
 
-app.get('/addOrEditSub', function(req, res){
+app.get("/addOrEditSub", function(req, res){
   var info = req.query;
-  db.collection('subs').findOne({link:info.link}, function(err, result) {
+  db.collection("subs").findOne({link:info.link}, function(err, result) {
     if(result){
       var temp = Object.keys(info);
       var key;
@@ -54,7 +57,7 @@ app.get('/addOrEditSub', function(req, res){
         key = temp[t];
         result[key] = info[key];
       }
-      db.collection('subs').save(result, function(err2) {
+      db.collection("subs").save(result, function(err2) {
         if (err2) {
           res.send("0");            
         } else {        
@@ -63,7 +66,7 @@ app.get('/addOrEditSub', function(req, res){
       }); 
     }
     else{
-      db.collection('subs').insert(info, function(err3, r3) {
+      db.collection("subs").insert(info, function(err3, r3) {
         if (err3) {
           res.send("0");            
         }
@@ -75,23 +78,23 @@ app.get('/addOrEditSub', function(req, res){
   });
 });
 
-app.get('/createUser', function(req, res){
+app.get("/createUser", function(req, res){
   userLib.add(req, res, db);
 });
 
-app.get('/editUser', function(req, res){
+app.get("/editUser", function(req, res){
   userLib.edit(req, res, db);
 });
 
-app.get('/changePassword', function(req, res){
+app.get("/changePassword", function(req, res){
   userLib.changePassword(req, res, db);
 });
 
-app.get('/login', function(req, res){
+app.get("/login", function(req, res){
   userLib.login(req, res, db);
 });
 
-app.get('/getUser', function(req, res){
+app.get("/getUser", function(req, res){
   userLib.get(req, res, db);
 });
 
@@ -100,24 +103,24 @@ function readURL(url, cb) {
   var protocol = url.split("://")[0];
   var request = require(protocol).get(url, function(res) {
 
-    res.on('data', function(chunk) {
+    res.on("data", function(chunk) {
       data += chunk;
     });
 
-    res.on('end', function() {
+    res.on("end", function() {
       console.log(JSON.parse(data))
       cb(data);
     })
   });
 
-  request.on('error', function(e) {
+  request.on("error", function(e) {
     console.log("Got error: " + e.message);
   });
 }
 
 app.use(methodOverride());
 app.use(bodyParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
