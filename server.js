ca518467ca9741561818a231bbd77ef6c6f01999
Jonatan Lemos Zuluaga (Student) <jonatan.lemos.z@gmail.com>
2016-04-06 00:35:59 -0700

    Trying port 8081.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var errorHandler = require("errorhandler");
 var methodOverride = require("method-override");
 var hostname = process.env.HOSTNAME || "localhost";
-var port = 8080;
+var port = 8081;
 var db = require("./node_modules/mongoskin").db("mongodb://user:password@127.0.0.1:27017/RSSFEED");
 
 app.use(session({secret: "This is a secret"}));
